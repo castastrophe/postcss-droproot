e16946b391cb8e5f66b1bbba17c1a4acc5771bc8
castastrophe <castastrophe@users.noreply.github.com>
2023-07-07 16:16:35 -0400

    feat: add a withFallback flag to retain custom property values

--- README.md
+++ README.md
@@ -11,7 +11,7 @@ postcss -u postcss-droproot -o dist/index.css src/index.css
 
 ## Usage
 
-Let's say you have `:root {}` rules you want gone. This plugin turns this:
+Let's say you have `:root` rules that you would like stripped out of your compiled output. This plugin turns this:
 
 ```css
 :root {
@@ -31,7 +31,38 @@ Into this:
 }
 ```
 
+## Options
+
+### `withFallback`
+
+If you wish to preserve the custom property definitions as fallback values in the output, set this flag to `true`. Note that this does not replace the custom property usage with the fallback value, it only adds the fallback value to the custom property definition. If a fallback value already exists, it will **not** be overwritten.
+
+```js
+postcss([
+  require('postcss-droproot')({
+    withFallback: true
+  })
+])
+```
+
+```css
+:root {
+  --prefix-component-background-color: blue;
+}
+
+.my-class {
+  background-color: var(--prefix-component-background-color);
+}
+```
+
+Into this:
+
+```css
+.my-class {
+  background-color: var(--prefix-component-background-color, blue);
+}
+```
+
 ## TODO
 
-- [ ] Add flag to keep fallback values for variables defined at root
 - [ ] Support `:root` prefixed selectors by dropping the prefix and retaining the rest of the selector
--- index.js
+++ index.js
@@ -10,14 +10,78 @@ OF ANY KIND, either express or implied. See the License for the specific languag
 governing permissions and limitations under the License.
 */
 
+/** @type import('postcss-values-parser') */
+const { parse, nodeToString } = require('postcss-values-parser');
+
 /** @type import('postcss').PluginCreator */
-module.exports = () => {
+module.exports = ({
+  withFallbacks = false,
+}) => {
   return {
     postcssPlugin: 'postcss-droproot',
-    Rule(rule) {
-      if (rule.selector === ':root') {
-        rule.remove();
-      }
+    prepare() {
+      const rootDefinitions = new Map();
+      return {
+        Declaration(decl, { result }) {
+          const selector = decl.parent.selector;
+          if (!withFallbacks || selector !== ':root') return;
+          // If this is a :root selector, capture fallback values
+          if (decl.prop.startsWith('--')) {
+            // Add the fallback value to the map, we always want to keep the last value
+            rootDefinitions.set(decl.prop, decl.value);
+            return;
+          }
+
+          // @todo: If this is not a custom property, how do we want to handle it?
+          decl.warn(result, 'Only custom properties can be preserved from :root', {
+            word: decl.prop,
+          });
+        },
+        RuleExit(rule) {
+          const selector = rule.selector;
+
+          if (selector !== ':root') return;
+          rule.remove();
+        },
+        DeclarationExit(decl) {
+          if (!withFallbacks) return;
+
+          let shouldUpdate = false;
+
+          // On exit, if the selector is not root, check if it needs any fallbacks added
+          // If the value does not include a custom property, we don't need to add a fallback
+          const updates = parse(decl.value).nodes?.map((node) => {
+            if (!node.isVar) return node;
+
+            const [customProperty, , fallback] = node.nodes;
+
+            if (
+              (fallback && fallback.type === 'word') ||
+              !customProperty ||
+              !customProperty.value ||
+              !rootDefinitions.has(customProperty.value)
+            ) {
+              return node;
+            }
+
+            const fallbackValue = rootDefinitions.get(customProperty.value);
+
+            // Update the node to include the original custom property with the found fallback value
+            node.nodes.push(
+              { type: 'punctuation', value: ',', raws: { before: '' } },
+              { type: 'word', value: fallbackValue, raws: { before: ' ' } },
+            );
+
+            shouldUpdate = true;
+            return node;
+          });
+
+          // If any changes were made, we update the declaration
+          if (!shouldUpdate) return;
+
+          decl.value = updates.map(nodeToString).join(' ');
+        },
+      };
     },
   };
 };
