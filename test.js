b927cf45de05ba5c29a9111ee49973c2fae09e1a
castastrophe <castastrophe@users.noreply.github.com>
2023-07-07 16:16:35 -0400

    feat: add a withFallback flag to retain custom property values

--- test.js
+++ test.js
@@ -3,7 +3,10 @@ const test = require('ava');
 const postcss = require('postcss');
 const plugin = require('./index.js');
 
-function compare(t, fixtureFilePath, expectedFilePath, options = {}){
+function compare(t, fixtureFilePath, expectedFilePath, options = {}, {
+  warningCount = 0,
+  warningContent = [],
+} = {}){
   return postcss([ plugin(options) ])
     .process(
       readFile(`./fixtures/${fixtureFilePath}`),
@@ -13,7 +16,13 @@ function compare(t, fixtureFilePath, expectedFilePath, options = {}){
       const expected = result.css;
       const actual = readFile(`./expected/${expectedFilePath}`);
       t.is(expected, actual);
-      t.is(result.warnings().length, 0);
+      t.is(result.warnings().length, warningCount);
+
+      if (warningCount > 0) {
+        result.warnings().forEach((warning, index) => {
+          t.is(warning?.text, warningContent[index]);
+        });
+      }
     });
 }
 
@@ -23,4 +32,16 @@ function readFile(filename) {
 
 test('drop :root selector', t => {
   return compare(t, 'basic.css', 'basic.css', {});
+});
+
+test('capture fallback definitions from :root', t => {
+  return compare(t, 'with-fallback.css', 'with-fallback.css', {
+    withFallbacks: true,
+  });
+});
+
+  test('warn if withFallback is true and a non-variable property is found in :root', t => {
+    return compare(t, 'with-fallback-warning.css', 'with-fallback-warning.css', {
+      withFallbacks: true,
+    }, { warningCount: 1, warningContent: ['Only custom properties can be preserved from :root'] });
 });
